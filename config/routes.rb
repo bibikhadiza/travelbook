7418492beed2fa753818c50f8170f467fde54cfe
Bibi Khadiza <limakhadiza@yahoo.com>
2016-10-23 12:07:09 -0400

    fixing nested form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+
+  resources :destinations
+  post '/destinations/:id/edit' => "destinations#edit"
+  resources :pins
+  resources :posts
+  get '/destinations/:id/new' => "destinations#new"
+
+  # resources :destinations, only: [:show] do
+  #   resources :posts, only: [:edit]
+  # end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  root to: "application#home"
+  get '/users' => "users#index"
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
