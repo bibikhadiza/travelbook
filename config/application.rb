0078383fa8736f2ff61e4a071310405dede4a368
Bibi Khadiza <limakhadiza@yahoo.com>
2017-04-03 22:55:46 -0400

    finished compiling assets

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,8 @@ Bundler.require(*Rails.groups)
 #     @app.call(env)
 #   end
 # end
-# config.assets.initialize_on_precompile = false
-RAILS_ENV=production bundle exec rake assets:precompile
+
+
 
 
 module Travelbook
